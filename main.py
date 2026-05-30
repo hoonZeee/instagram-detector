42544a494964b10ce8d6696439155913540f8943
Jihoon <dlwlgns7540@naver.com>
2026-05-31 00:53:53 +0900

    async 버그 픽스

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import sys
+import os
+
+# PyInstaller 번들 환경에서 Playwright Chromium 경로 지정
+if getattr(sys, 'frozen', False):
+    _app_dir = os.path.dirname(sys.executable)
+    os.environ['PLAYWRIGHT_BROWSERS_PATH'] = os.path.join(_app_dir, 'pw-browsers')
+
+# Windows에서 Playwright Sync API 와 asyncio ProactorEventLoop 충돌 방지
+if sys.platform == "win32":
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 import customtkinter as ctk
 from gui import App
 
